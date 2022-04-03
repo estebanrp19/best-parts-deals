2fd4e561313847fd7210b81d8caedc8d68d251d5
Azlain Saavedra <azlain.saavedra@gmail.com>
2022-04-03 18:53:58 -0500

    fix count description item limit on order

--- js/components/item-on-order.js
+++ js/components/item-on-order.js
@@ -1,8 +1,15 @@
 const itemOnOrder = (data) => {
+
+    let description = data.DESCRIPCION.substring(0, 40);
+
+    if (description.length >= 40) {
+        description = description + '...'
+    }
+
     const template = '<div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-3" id="container-order-item-' + data.NUM_REG + '">' +
         '<div class="box-items-order" id="item-order-card-' + data.CODIGO + '">' +
         '<div class="card-body">' +
-        '<h5 class="card-tittle">' + data.APLICACION + ' - ' + data.DESCRIPCION + '</h5>' +
+        '<h5 class="card-tittle">' + description + '</h5>' +
         '<p class="card-subtitle text-muted">$' + data.PRECIO1 + '</p>' +
         '<h6 class="text-secondary">Code: ' + data.CODIGO + '</h6>' +
         '<label>Quantity: <span id="order-item-' + data.NUM_REG + '">' + data.qty + '</span></label>' +
