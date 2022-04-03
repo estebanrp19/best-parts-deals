c592315d07ad0da85e143313d4329bfabdfe561b
Azlain Saavedra <azlain.saavedra@gmail.com>
2022-04-03 18:22:23 -0500

    limit length description items

--- js/components/item-on-products-list.js
+++ js/components/item-on-products-list.js
@@ -1,9 +1,12 @@
 const itemOnProductsList = (data) => {
+
+    const description = data.DESCRIPCION.substring(0, 60) + '...';
+
     const template = ' <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-3 ">' +
         '<div class="card">' +
         '<img class="card-img-top" src="' + BPD_IMAGES_URL + data.CODIGO + '.jpg" alt="">' +
         '<div class="card-body">' +
-        '<h5 class="card-tittle">' + data.APLICACION + ' - ' + data.DESCRIPCION + '</h5>' +
+        '<h5 class="card-tittle">' + description + '</h5>' +
         '<p class="card-subtitle text-muted">$' + data.PRECIO1 + '</p>' +
         '<h6 class="text-success">Available: ' + data.EXISTENCIA + '</h6>' +
         '<h6 class="text-secondary">Code: ' + data.CODIGO + '</h6>' +
