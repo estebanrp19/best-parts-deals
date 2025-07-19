d022a2b25d15fa5483af02345fc2bc14059959f8
sazlain <azlain.saavedra@gmail.com>
2025-07-18 22:44:11 -0500

    Fix redirect to localhost

--- resources/constants.js
+++ resources/constants.js
@@ -15,7 +15,7 @@ const constants = {
         "GET_CAR_MODELS_LIST": "SELECT  DISTINCT (SPLIT_STR(paa.APLICACION,' ', 3)) model from PMT_APLICACIONES_ACT paa inner join PMT_MARKETPLACES pm on paa.CODIGO = pm.CODIGO where pm.MARKETPLACE = '004' and SPLIT_STR(paa.APLICACION,' ', 2) = '@@0' ORDER by model ASC",
         "GET_APPLICATION_PART_LIST": "SELECT DISTINCT (paa.APLICACION), paa.YEAR_INI, paa.YEAR_FIN from PMT_APLICACIONES_ACT paa inner join PMT_MARKETPLACES pm on paa.CODIGO = pm.CODIGO where pm.MARKETPLACE = '004' ORDER by APLICACION ASC",
     },
-    WORKFLOW_BASE_URL: "http://24.10.191.13:8083",
+    WORKFLOW_BASE_URL: "http://24.10.191.13:8083", 
     WORKFLOW_API_REST: {
         VALIDATE_EXISTENCE: "/pm/validate-existence",
         UPDATE_PRERESERVA: "/pm/update-pre-reserva",
